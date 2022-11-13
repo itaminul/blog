b7d796bdcb94aa543e2f46bb3455c0ff55d11e9a
itaminul@gmail.com <itaminul@gmail.com>
2022-11-13 23:20:26 +0600

    data table add

--- src/components/pages/setup/batch/BatchIndex.js
+++ src/components/pages/setup/batch/BatchIndex.js
@@ -1,44 +1,47 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import SideBar from '../../../layouts/SideBar'
-import { Table } from './Table'
+import  Table  from './Table'
 import { useSelector } from 'react-redux'
 
 const BatchIndex = () => {
 
     const open = useSelector((state) => state.sidebar.open)
+    
+    const [data, setData] = useState([])
 
-    const data = useMemo(
-      () => [
-        {
-          col1: 'Hello',
-          col2: 'World',
-        },
-        {
-          col1: 'react-table',
-          col2: 'rocks',
-        },
-        {
-          col1: 'whatever',
-          col2: 'you want',
-        },
-      ],
-      []
-    )
-  
+    useEffect(() => {
+      fetch('/MOK_DATA.json', {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      })
+      .then(function(response){
+        //console.log(response)
+        return response.json();
+      })
+      .then(function(data) {
+        setData(data);
+      });
+    }, [])
+
     const columns = useMemo(
       () => [
         {
-          Header: 'Column 1',
-          accessor: 'col1', // accessor is the "key" in the data
+          Header: 'ID',
+          accessor: 'id', // accessor is the "key" in the data
         },
         {
-          Header: 'Column 2',
-          accessor: 'col2',
+          Header: 'Name',
+          accessor: 'name', // accessor is the "key" in the data
+        },
+        {
+          Header: 'Address',
+          accessor: 'address', // accessor is the "key" in the data
         },
       ],
       []
     )
-
   return (
     <div style={open == true ? {marginTop: "70px", marginLeft: "250px"} : {marginTop: "70px", marginLeft: "76px"}}>
         <SideBar />     
