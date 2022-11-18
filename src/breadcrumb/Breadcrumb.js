f743822481ed38f6a56c44f582bca8b439fb9aa3
itaminul@gmail.com <itaminul@gmail.com>
2022-11-18 16:00:24 +0600

    modification breadcrumbs features

--- src/breadcrumb/Breadcrumb.js
+++ src/breadcrumb/Breadcrumb.js
@@ -12,12 +12,12 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Popper from '@mui/material/Popper';
 import Fade from '@mui/material/Fade';
-
-
-
-import ModalForm from '../modal/ModalForm'
-
-
+import List from '@mui/joy/List';
+import ListItem, { listItemClasses } from '@mui/joy/ListItem';
+import ReceiptLong from '@mui/icons-material/ReceiptLong';
+import IconButton from '@mui/joy/IconButton';
+import ListItemButton, { listItemButtonClasses } from '@mui/joy/ListItemButton';
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -25,47 +25,200 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
-
 const breacrambStyle = {
-  background: '#a6a6a6',
+  border: '2px solid black',
   color: 'black',
   marginTop: '-10px',
   marginLeft: '10px',
   height: '40px'
-
 }
-
 const featuresStyle = {
-  border: 'none',
+  width: '250px',
+  marginLeft: '1030px',
+  marginTop: '2px',
+  border: '2px solid black',
   textAlign: 'center',
-  color: 'white',
-  background: '#dc3545',
+  color: 'black',
   borderRadius: '10px',
   padding: '5px 15px 10px 15px',
+  borderBottomRightRadius: '50px',
 }
 const Breadcrumb = (props) => {
-
-
-const [anchorEl, setAnchorEl] = useState(null);
+  const [openb, setOpenb] = React.useState(false);
+  const [open2, setOpen2] = React.useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState();
-
   const handleClick = (newPlacement) => (event) => {
     setAnchorEl(event.currentTarget);
     setOpen((prev) => placement !== newPlacement || !prev);
     setPlacement(newPlacement);
-  };
-
+  }; 
+  const closeProper = (e) => {
+    e.preventDefault()
+    setOpen(false);
+  }
      return (
     <div role="presentation">
-
     <Box sx={{ flexGrow: 1 }}>
       <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+       
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
             <Paper style={featuresStyle}>
-              <Typography sx={{ p: 2 }}>The content of the Popper ddd.</Typography>
+              <Typography sx={{ p: 2 }}>
+              <List
+        size="sm"
+        sx={(theme) => ({
+          // Gatsby colors
+          '--joy-palette-primary-plainColor': '#8a4baf',
+          '--joy-palette-neutral-plainHoverBg': 'transparent',
+          '--joy-palette-neutral-plainActiveBg': 'transparent',
+          '--joy-palette-primary-plainHoverBg': 'transparent',
+          '--joy-palette-primary-plainActiveBg': 'transparent',
+          [theme.getColorSchemeSelector('dark')]: {
+            '--joy-palette-text-secondary': '#635e69',
+            '--joy-palette-primary-plainColor': '#d48cff',
+          },
+
+          '--List-insetStart': '32px',
+          '--List-item-paddingY': '0px',
+          '--List-item-paddingRight': '16px',
+          '--List-item-paddingLeft': '21px',
+          '--List-item-startActionWidth': '0px',
+          '--List-item-startActionTranslateX': '-50%',
+
+          [`& .${listItemButtonClasses.root}`]: {
+            borderLeft: '1px solid',
+            borderColor: 'divider',
+          },
+          [`& .${listItemButtonClasses.root}.${listItemButtonClasses.selected}`]: {
+            borderColor: 'currentColor',
+          },
+          [`& .${listItemClasses.nested} > .${listItemButtonClasses.root}`]: {
+            border: 'none',
+          },
+          '& [class*="startAction"]': {
+            color: 'var(--joy-palette-text-tertiary)',
+          },
+        })}
+      >
+        <ListItem nested>
+          <ListItem component="div" startAction={<ReceiptLong />}>
+            <Typography level="body3" sx={{ textTransform: 'uppercase' }}>
+              Documentation
+            </Typography>
+          </ListItem>
+        </ListItem>
+        <ListItem
+          nested
+          sx={{ my: 1 }}
+          startAction={
+            <IconButton
+              variant="plain"
+              size="sm"
+              color="neutral"
+              onClick={() => setOpenb(!openb)}
+            >
+              <KeyboardArrowDown
+                sx={{ transform: open ? 'initial' : 'rotate(-90deg)' }}
+              />
+            </IconButton>
+          }
+        >
+          <ListItem>
+            <Typography
+              level="inherit"
+              sx={{
+                fontWeight: open ? 'bold' : undefined,
+                color: open ? 'text.primary' : 'inherit',
+              }}
+            >
+              Datatable
+            </Typography>
+          </ListItem>
+          {openb && (
+            <List sx={{ '--List-item-paddingY': '8px' }}>
+              <ListItem>
+                <ListItemButton>TanStack Table </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                 Version 7
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                 Json Data
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                 Column Filter
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                Pagination
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                 Data Counting
+                </ListItemButton>
+              </ListItem>
+            </List>
+          )}
+        </ListItem>
+        <ListItem
+          nested
+          sx={{ my: 1 }}
+          startAction={
+            <IconButton
+              variant="plain"
+              size="sm"
+              color="neutral"
+              onClick={() => setOpen2((bool) => !bool)}
+            >
+              <KeyboardArrowDown
+                sx={{ transform: open2 ? 'initial' : 'rotate(-90deg)' }}
+              />
+            </IconButton>
+          }
+        >
+          <ListItem>
+            <Typography
+              level="inherit"
+              sx={{
+                fontWeight: open2 ? 'bold' : undefined,
+                color: open2 ? 'text.primary' : 'inherit',
+              }}
+            >
+              Modal Form
+            </Typography>
+          </ListItem>
+          {open2 && (
+            <List sx={{ '--List-item-paddingY': '8px' }}>
+              <ListItem>
+                <ListItemButton>Show Batch List</ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>Create Batch With Modal Form</ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>Edit Batch With Modal Form</ListItemButton>
+              </ListItem>
+            </List>
+          )}
+        </ListItem>
+      </List>              
+              </Typography>
+               <Typography>
+                <Button variant="outlined" onClick={closeProper}>Close</Button>
+                
+                </Typography>
             </Paper>
+            
           </Fade>
         )}
       </Popper>
@@ -92,16 +245,14 @@ const [anchorEl, setAnchorEl] = useState(null);
       </Breadcrumbs>
         </Grid>
         <Grid xs={1}>
-        <Button onClick={handleClick('bottom')}>bottom</Button>
+        <Button style={{marginTop: '-8px', marginLeft: '9px', background: 'black', color: 'white'}} onClick={handleClick('bottom')}>Features</Button>
         </Grid>
         <Grid xs={1}>
         {props.modalForm}
         </Grid>
         
       </Grid>
-    </Box>
-
-      
+    </Box>      
     </div>
   );
 }
