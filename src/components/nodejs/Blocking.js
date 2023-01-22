b2e462e7d4bf1203c1f9e8f39bc3741538218f0b
itaminul@gmail.com <itaminul@gmail.com>
2023-01-22 13:36:49 +0600

    left side bar menu title

--- src/components/nodejs/Blocking.js
+++ src/components/nodejs/Blocking.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import TopBar from '../layouts/TopBar'
 import Breadcrumbs from '../layouts/Breadcrumbs'
+import LeftBarTitle from '../layouts/LeftBarTitle'
 import WhatisjavascriptDetails from '../javascript/WhatisjavascriptDetails'
 import ReadMoreLeft from '../readMore/nodejsLayouts/ReadMoreLeft'
 import {
     Container,
     DetailsContent,
     RightMenu,
-    LeftMenu
+    LeftMenu,
+    LeftMTitle
 } from '../javascript/Common.styled'
 
 const Blocking = (props) => {
@@ -19,6 +21,9 @@ const Blocking = (props) => {
         bread={props.breadcrumbs.arrowFunction} />
     <Container>
         <LeftMenu>
+        <LeftMTitle>
+                       <LeftBarTitle leftbartitle={props.leftbartitle.javascriptTitle} />
+                    </LeftMTitle>
          <ReadMoreLeft/>
     </LeftMenu>
         <DetailsContent>            
