e9a3f246af1f3c44e9203cddd307ce6fc38ad908
itaminul <itaminul@gmail.com>
2023-06-08 22:28:35 +0600

    update

--- src/components/readMore/ReadMoreContent.js
+++ src/components/readMore/ReadMoreContent.js
@@ -21,6 +21,7 @@ const ReadMoreContent = () => {
       <DetailsDescription>      
       <button onClick={clickHandler}>Change Title</button>
       </DetailsDescription>
+      
     </Details>  
    </Container>    
    </>
